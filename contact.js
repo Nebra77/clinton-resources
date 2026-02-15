39def2842b178470b54e018e0146769ce3834843
Nebra77 <arbenhusenaj09@gmail.com>
2026-02-15 14:56:02 -0500

    Begin creating contact page

---
+++ contact.js
@@ -0,0 +1,50 @@
+function addings(){
+    const filler = document.getElementById("fill")
+    if(filler.textContent.trim()==='' && filler.textContent.trim()!=="Send a message"){
+        adding();
+    }
+    else if(filler.textContent.trim()==="Send a message"){
+        (document.getElementById("s")).remove();
+        (document.getElementById("q")).remove();
+        adding();
+    }
+}
+
+function adding(){
+    const title = document.createElement("h2");
+    const eventName = document.createElement("input");
+    const eventDate = document.createElement("input");
+    const br = document.createElement("br")
+    title.textContent = "Add a resource";
+    title.id = "s"
+    eventName.placeholder = "Event Name";
+    eventName.id = "q"
+    eventDate.placeholder = "Date of Event"
+    document.getElementById("fill").append(title);
+    document.getElementById("fill").append(eventName);
+    document.getElementById("fill").append(br);
+    document.getElementById("fill").append(eventDate);
+}
+
+function contactings(){
+    const filler = document.getElementById("fill")
+    if(filler.textContent.trim()==='' && filler.textContent.trim()!=="Add a resource"){
+        contacting()
+    }
+    else if(filler.textContent.trim()!==""){
+        (document.getElementById("s")).remove();
+        (document.getElementById("q")).remove();
+        contacting();
+    }
+}
+
+function contacting(){
+    const title = document.createElement("h2");
+    const eventName = document.createElement("input");
+    title.textContent = "Send a message";
+    title.id = "s";
+    eventName.placeholder = "Event Name";
+    eventName.id = "q";
+    document.getElementById("fill").append(title);
+    document.getElementById("fill").append(eventName)
+}
