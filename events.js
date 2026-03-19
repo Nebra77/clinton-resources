d42b0b1c65ab3aaa41c4fea88a889de20f651cb1
Nebra77 <arbenhusenaj09@gmail.com>
2026-03-19 19:34:41 -0400

    Begin work on class in events.js

---
+++ events.js
@@ -0,0 +1,10 @@
+class Event {
+    constructor(title, start, end, location) {
+        this.title = title;
+        this.start = start;
+        this.end = end;
+        this.location = location;
+    }
+}
+
+const stPatricks = new Event("Saint Patricks Day Parade","2026-3-")
