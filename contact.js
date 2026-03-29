c86e36ddbad904b75cb510775fabbfda8ded82a9
Nebra77 <arbenhusenaj09@gmail.com>
2026-03-28 20:13:53 -0400

    Revamp contact.html javascript

--- contact.js
+++ contact.js
@@ -1,69 +1,44 @@
-function addings(){
-    const filler = document.getElementById("fill")
-    if(filler.textContent.trim()==='' && filler.textContent.trim()!=="Send a message"){
-        adding();
-    }
-    else if(filler.textContent.trim()==="Send a message"){
-        filler.replaceChildren();
-        adding();
-    }
-}
+const add = document.getElementById("adding");
+const messageForm = document.getElementById("messageForm");
 
-function adding(){
-    const title = document.createElement("h2");
-    const eventName = document.createElement("input");
-    const eventDate = document.createElement("input");
-    const oversee = document.createElement("input");
-    const desc = document.createElement("textarea")
-    const sub = document.createElement("button");
-    title.textContent = "Add a resource";
-    title.id = "s";
-    eventName.placeholder = "Resource/Event Name";
-    eventName.id = "eventName";
-    eventName.name = "eventName"
-    eventDate.placeholder = "Date of Event (Na if none)";
-    eventDate.id = "dateOf";
-    eventDate.name = "dateOf";
-    oversee.placeholder = "Organization name"
-    oversee.id = "oversee";
-    oversee.name = "oversee";
-    desc.placeholder = "Add a short description";
-    desc.id = "desc";
-    desc.name = "desc";
-    sub.id = "subAdd";
-    sub.textContent = "Submit";
-    sub.type = "submit";
-    document.getElementById("fill").append(title);
-    document.getElementById("fill").append(eventName);
-    document.getElementById("fill").append(document.createElement("br"));
-    document.getElementById("fill").append(eventDate);
-    document.getElementById("fill").append(document.createElement("br"));
-    document.getElementById("fill").append(oversee);
-    document.getElementById("fill").append(document.createElement("br"));
-    document.getElementById("fill").append(desc);
-    document.getElementById("fill").append(document.createElement("br"));
-    document.getElementById("fill").append(sub);
-}
+function addings(){
+    if(add.style.display === ""){
+        add.style.display = 'block';
+        messageForm.style.display = "";
+    };
+};
+
+
 
 function contactings(){
-    const filler = document.getElementById("fill")
-    if(filler.textContent.trim()==='' && filler.textContent.trim()!=="Add a resource"){
-        contacting()
-    }
-    else if(filler.textContent.trim()!==""){
-        filler.replaceChildren();
-        contacting();
-    }
+    if(messageForm.style.display === ""){
+        messageForm.style.display = 'block';
+        add.style.display = "";
+
+    };
 }
 
 function contacting(){
     const title = document.createElement("h2");
-    const eventName = document.createElement("input");
+    const mess = document.createElement("input");
+    const resp = document.createElement("input");
+    const sub = document.createElement("button");
     title.textContent = "Send a message";
-    title.id = "s";
-    eventName.placeholder = "Message";
-    eventName.id = "q";
+    title.id = "titleB";
+    mess.placeholder = "Message";
+    mess.id = "mess";
+    mess.name = "mess";
+    resp.placeholder = "Contact info:";
+    resp.id = "resp";
+    resp.name = "resp";
+    sub.textContent = "Submit";
+    sub.id = "subAdd";
+    sub.type = "submit";
     document.getElementById("fill").append(title);
-    document.getElementById("fill").append(eventName)
+    document.getElementById("fill").append(mess);
+    document.getElementById("fill").append(document.createElement("br"));
+    document.getElementById("fill").append(resp);
+    document.getElementById("fill").append(document.createElement("br"));
+    document.getElementById("fill").append(sub);
 }
 
--- contactSub.js
+++ contactSub.js
@@ -1,11 +1,14 @@
-document.querySelector("#fill").addEventListener("submit", async (e) => {
-  e.preventDefault();
-  const formData = {
-    eventName: e.target.eventName.value,
-    dateOf: e.target.dateOf.value,
-    oversee: e.target.oversee.value,
-    desc: e.target.desc.value,
-  };
+document.querySelector("#adding").addEventListener("submit", async (e) => {
+    e.preventDefault();
+      const formData = {
+        type: "Addition",
+        eventName: e.target.eventName.value,
+        dateOf: e.target.dateOf.value,
+        oversee: e.target.oversee.value,
+        desc: e.target.desc.value,
+        contact: e.target.contactAdd.value,
+
+    };
   try {
     const res = await fetch("http://localhost:3000/submit", {
       method: "POST",
@@ -22,3 +25,26 @@ document.querySelector("#fill").addEventListener("submit", async (e) => {
   }
 });
 
+document.querySelector("#messageForm").addEventListener("submit", async (e) => {
+    e.preventDefault();
+      const formData = {
+        type: "Message",
+        mess: e.target.mess.value,
+        contact: e.target.contactMess.value,
+
+    };
+  try {
+    const res = await fetch("http://localhost:3000/submit", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(formData)
+    });
+
+    alert("Submitted!");
+  } catch (err) {
+    alert("Something went wrong");
+    console.error(err);
+  }
+});
